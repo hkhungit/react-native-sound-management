a6a7429aa53fd930a62ac54dec5d154057670df5
Jim Fwz <jim@futureworkz.com>
2016-09-16 13:11:36 +0700

    set up

--- index.android.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import {AppRegistry} from 'react-native';
-import ReactNativeSoundManagement from './app/Media';
-
-AppRegistry.registerComponent('ReactNativeSoundManagement', () => ReactNativeSoundManagement);
---
+++ index.js
@@ -0,0 +1,4 @@
+import MediaStates    from './app/MediaStates'
+import SoundPlayer    from './app/SoundPlayer'
+import SoundRecorder  from './app/SoundRecorder'
+export default { MediaStates, SoundPlayer, SoundRecorder }
