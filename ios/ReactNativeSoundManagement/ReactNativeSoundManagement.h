f7a081efa2c36f693b121795abaabfcc3f871fa3
Jim Fwz <jim@futureworkz.com>
2016-09-19 16:10:17 +0700

    add lib ios

---
+++ ios/ReactNativeSoundManagement/ReactNativeSoundManagement.h
@@ -0,0 +1,13 @@
+//
+//  ReactNativeSoundManagement.h
+//  ReactNativeSoundManagement
+//
+//  Created by Jim Fwz on 9/19/16.
+//  Copyright © 2016 Jim Fwz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ReactNativeSoundManagement : NSObject
+
+@end
---
+++ ios/ReactNativeSoundManagement/ReactNativeSoundManagement.m
@@ -0,0 +1,13 @@
+//
+//  ReactNativeSoundManagement.m
+//  ReactNativeSoundManagement
+//
+//  Created by Jim Fwz on 9/19/16.
+//  Copyright © 2016 Jim Fwz. All rights reserved.
+//
+
+#import "ReactNativeSoundManagement.h"
+
+@implementation ReactNativeSoundManagement
+
+@end
